930a75fb3461b7fbe706f5f09b059a41099a02cc
Jan Velema <velema98@gmail.com>
2017-02-03 17:06:34 +0100

    Added prefabs and started on spawnpoint

---
+++ Action_Platformer/Assets/SpawnPoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+    public GameObject player;
+    
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        //If there is no child (player) spawn a player and make it a child
+        if (transform.childCount == 0)
+        {
+            GameObject playerClone = (GameObject)Instantiate(player, transform.position, Quaternion.identity);
+            playerClone.transform.parent = gameObject.transform;
+        }
+	}
+}
