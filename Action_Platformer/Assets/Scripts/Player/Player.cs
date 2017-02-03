1b69f7100fbc674563267484a4ac321d3da44908
Jan Velema <velema98@gmail.com>
2017-02-03 15:47:36 +0100

    Raycast jumping checks
    Checks if on surface using raycast to prevent jumping in air and allow walljump

--- Action_Platformer/Assets/Scripts/Player/Player.cs
+++ Action_Platformer/Assets/Scripts/Player/Player.cs
@@ -7,16 +7,36 @@ public class Player : MonoBehaviour {
     public float jump;
     public float maxSpeed;
     Rigidbody rb;
-    
 
-	// Use this for initialization
-	void Start () {
+    bool hitGround;
+    float RaycastDistGround = 0.5f;
+    bool hitLeftWall;
+    bool hitRightWall;
+    float RaycastDistWall = 0.5f;
+    public LayerMask GroundLayer;
+
+
+
+    // Use this for initialization
+    void Start () {
         rb = gameObject.GetComponent<Rigidbody>();
         jump *= 100;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        hitGround = Physics.Raycast(transform.position, Vector3.down, RaycastDistGround, GroundLayer);
+        hitLeftWall = Physics.Raycast(transform.position, Vector3.left, RaycastDistGround, GroundLayer);
+        hitRightWall = Physics.Raycast(transform.position, Vector3.right, RaycastDistGround, GroundLayer);
+
+
+        Debug.Log(hitGround);
+        Debug.Log(hitLeftWall);
+        Debug.Log(hitRightWall);
+        
+
         if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
         {
 
@@ -29,9 +49,21 @@ public class Player : MonoBehaviour {
         {
             rb.AddForce(speed, 0, 0);
         }
+        
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            rb.AddForce(0, jump, 0);
+            if (hitGround)
+                rb.AddForce(0, jump, 0);
+            else if ((hitLeftWall) && (Input.GetKey(KeyCode.LeftArrow)))
+                rb.AddForce(0, jump, 0);
+            else if ((hitRightWall) && (Input.GetKey(KeyCode.RightArrow)))
+                rb.AddForce(0, jump, 0);
+        }
+
+
+        if ((hitGround) && (rb.velocity.y < 0))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
         }
     }
 
