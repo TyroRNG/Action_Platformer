ee8054ddf7636503d8c68b30005a6a89d2154330
Jan Velema <velema98@gmail.com>
2017-02-03 16:38:20 +0100

    Directional collision detection, clipping prevention and comments

--- Action_Platformer/Assets/Scripts/Player/Player.cs
+++ Action_Platformer/Assets/Scripts/Player/Player.cs
@@ -3,16 +3,28 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+    //Basic movement variables
     public float speed;
     public float jump;
     public float maxSpeed;
     Rigidbody rb;
 
+    //Frame timer for jumping to feel more resposive
+    int upPressedTimer = 0;
+    int checkDelay = 5;
+
+    //Variables for directional collision detection
+    //Down collision detection
     bool hitGround;
     float RaycastDistGround = 0.5f;
+    //Up collision detection
+    bool hitCeiling;
+    float RaycastDistCeiling = 0.5f;
+    //Left and Right collision detection
     bool hitLeftWall;
     bool hitRightWall;
     float RaycastDistWall = 0.5f;
+    //Only detect objects marked als ground blocks
     public LayerMask GroundLayer;
 
 
@@ -20,27 +32,32 @@ public class Player : MonoBehaviour {
     // Use this for initialization
     void Start () {
         rb = gameObject.GetComponent<Rigidbody>();
-        jump *= 100;
+        //jump *= 100;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        //Check if touching ground, wall and/or ceiling and direction
         hitGround = Physics.Raycast(transform.position, Vector3.down, RaycastDistGround, GroundLayer);
+        hitCeiling = Physics.Raycast(transform.position, Vector3.up, RaycastDistCeiling, GroundLayer);
         hitLeftWall = Physics.Raycast(transform.position, Vector3.left, RaycastDistGround, GroundLayer);
         hitRightWall = Physics.Raycast(transform.position, Vector3.right, RaycastDistGround, GroundLayer);
 
+        /*
+        Collsion checks debuging
 
         Debug.Log(hitGround);
+        Debug.Log(hitCeiling);
         Debug.Log(hitLeftWall);
         Debug.Log(hitRightWall);
-        
+        */
 
+        //Prevent double controles
         if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
-        {
-
-        }
+        {}
+        //Movement on X
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.AddForce(-speed,0,0);
@@ -49,26 +66,33 @@ public class Player : MonoBehaviour {
         {
             rb.AddForce(speed, 0, 0);
         }
-        
+
+        //Jump Controles
+        //only check on frame when pressed to avoid unnessesary jumping
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            if (hitGround)
-                rb.AddForce(0, jump, 0);
-            else if ((hitLeftWall) && (Input.GetKey(KeyCode.LeftArrow)))
-                rb.AddForce(0, jump, 0);
+            //If on wall and pressing button towards that wall allow walljump
+            if ((hitLeftWall) && (Input.GetKey(KeyCode.LeftArrow)))
+                rb.velocity = new Vector3(jump, jump, rb.velocity.z);
             else if ((hitRightWall) && (Input.GetKey(KeyCode.RightArrow)))
-                rb.AddForce(0, jump, 0);
+                rb.velocity = new Vector3(-jump, jump, rb.velocity.z);
+            upPressedTimer = checkDelay;
         }
-
-
-        if ((hitGround) && (rb.velocity.y < 0))
+        //Check if jump pressed for more that one frame for nicer controling but preventing autojump with a timer
+        else if ((Input.GetKey(KeyCode.UpArrow)) && (upPressedTimer > 0))
         {
-            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            //Only allow jumping while on the ground
+            if (hitGround)
+                rb.velocity = new Vector3(rb.velocity.x, jump, rb.velocity.z);
         }
+        //Deplete upPressedTimer so that you cannot hold up
+        if (upPressedTimer > 0)
+            upPressedTimer--;
     }
 
     void FixedUpdate()
     {
+        //Set a max X speed to prevent lightspeed 
         if (rb.velocity.x > maxSpeed)
         {
             rb.velocity = new Vector3(maxSpeed, rb.velocity.y, rb.velocity.z);
@@ -77,5 +101,16 @@ public class Player : MonoBehaviour {
         {
             rb.velocity = new Vector3(-maxSpeed, rb.velocity.y, rb.velocity.z);
         }
+
+        //Prevent clipping using raycasting in all 4 directions
+        if ((hitLeftWall) && (rb.velocity.x < 0))
+            rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
+        if ((hitRightWall) && (rb.velocity.x > 0))
+            rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
+        if ((hitGround) && (rb.velocity.y < 0))
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        if ((hitCeiling) && (rb.velocity.y > 0))
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
     }
 }
